495abc45c13f48803b69a31e190c888829efbb80
max_shirokov <maksim.shirokov2011@mail.ru>
2021-12-30 19:06:59 +0300

    кофиги собраны

---
+++ index.js
@@ -0,0 +1,39 @@
+import { printOutput, printResult } from './src/js/output';
+import getDatesDiff from './src/js/getDatesDiff';
+import { initField, start} from './src/js/timer';
+import chooseFunc from './src/js/chooseModule';
+
+
+  const form = document.getElementById('datecalc');
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const { firstDate, secondDate } = form.elements;
+
+    if (!firstDate.value || !secondDate.value) {
+      printOutput('ошибка');
+
+      return;
+    }
+
+    const dateDiff = getDatesDiff({ from: firstDate.value, to: secondDate.value });
+
+    printResult(dateDiff);
+  };
+
+  const formTimer = initField('timer');
+
+  formTimer.onsubmit = (e) => {
+    e.preventDefault();
+  };
+
+  formTimer.addEventListener('click', (e) => {
+    if (e.target.className !== 'start') return;
+    start(formTimer.timer);
+  });
+
+  chooseFunc('chooseForm');
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: './index.js',
+  mode: 'production',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
